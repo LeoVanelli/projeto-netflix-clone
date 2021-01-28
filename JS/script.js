449fe37f4dc29a284ed76b61851c07655a3fb185
Leandro <leandro.vanelli@hotmail.com>
2021-01-28 20:30:33 -0300

    Netflix Interface 1.0

---
+++ JS/script.js
@@ -0,0 +1,7 @@
+function gambitoficial(){
+    window.open('https://www.netflix.com/br/title/80234304');
+}
+
+function trailer(){
+    window.open('https://www.youtube.com/watch?v=cnqV3wsZlpo');
+}
